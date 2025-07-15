b01aba3d0e24960a916ad110dad1a523ee38140b
war-julia <war-julia@yandex.ru>
2025-07-16 00:21:14 +0300

    first commit

---
+++ tests/test_check_icon_demoqa.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_check_icon():
+    driver=webdriver.Chrome()
+    driver.get('https://demoqa.com/')
+
+    icon=driver.find_element(By.CSS_SELECTOR, '#app>header>a')
+    if icon is None:
+        print('Не найден элемент')
+    else:
+        print('Найден')
