87cd3d2ba98c5661d7935131b06a56054be1e489
war-julia <war-julia@yandex.ru>
2025-07-16 08:21:32 +0300

    second commit

--- conftest.py
+++ conftest.py
@@ -1,3 +1,8 @@
-/venv
-.idea
-*
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope='session')
+def browser():
+    driver=webdriver.Chrome()
+    yield driver
+    driver.quit()
