cada7d212ca83bc43ca3efebecaf4181fce5bed1
war-julia <war-julia@yandex.ru>
2025-07-30 00:45:52 +0300

    first commit

--- tests/test_go_to_page_elements.py
+++ tests/test_go_to_page_elements.py
@@ -4,24 +4,24 @@ from pages.demoqa import SauceDemo
 from pages.elements_page import InventoryPage
 
 
-def test_add_item_to_cart(browser):
-    """Test adding item to cart functionality"""
-    sauce_demo_page = SauceDemo(browser)
-    inventory_page = InventoryPage(browser)
-    
-    # Login first
-    sauce_demo_page.visit()
-    sauce_demo_page.login('standard_user', 'secret_sauce')
-    
-    # Verify we're on inventory page
-    assert 'inventory.html' in sauce_demo_page.get_url()
-    
-    # Add first item to cart
-    inventory_page.add_item_to_cart(0)
-    
-    # Verify item was added (button text should change to "Remove")
-    buttons = browser.find_elements(By.CSS_SELECTOR, '.btn_inventory')
-    assert buttons[0].text == "Remove"
+# def test_add_item_to_cart(browser):
+#     """Test adding item to cart functionality"""
+#     sauce_demo_page = SauceDemo(browser)
+#     inventory_page = InventoryPage(browser)
+#
+#     # Login first
+#     sauce_demo_page.visit()
+#     sauce_demo_page.login('standard_user', 'secret_sauce')
+#
+#     # Verify we're on inventory page
+#     assert 'inventory.html' in sauce_demo_page.get_url()
+#
+#     # Add first item to cart
+#     inventory_page.add_item_to_cart(0)
+#
+#     # Verify item was added (button text should change to "Remove")
+#     buttons = browser.find_elements(By.CSS_SELECTOR, '.btn_inventory')
+#     assert buttons[0].text == "Remove"
 
 
 def test_logout_functionality(browser):
