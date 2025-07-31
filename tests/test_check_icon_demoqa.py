970c4859ec335cf781329544343c8410f1c72a30
war-julia <war-julia@yandex.ru>
2025-07-31 21:26:29 +0300

    first commit

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -2,16 +2,16 @@ import pytest
 from pages.demoqa import SauceDemo
 
 
-def test_login_with_valid_credentials(browser):
-    """Test login with valid credentials"""
-    sauce_demo_page = SauceDemo(browser)
-    sauce_demo_page.visit()
-    
-    # Login with standard user
-    sauce_demo_page.login('visual_user', 'secret_sauce')
-    
-    # Verify successful login by checking URL change
-    assert 'inventory.html' in sauce_demo_page.get_url()
+# def test_login_with_valid_credentials(browser):
+#     """Test login with valid credentials"""
+#     sauce_demo_page = SauceDemo(browser)
+#     sauce_demo_page.visit()
+#
+#     # Login with standard user
+#     sauce_demo_page.login('visual_user', 'secret_sauce')
+#
+#     # Verify successful login by checking URL change
+#     assert 'inventory.html' in sauce_demo_page.get_url()
 
 
 def test_login_with_invalid_credentials(browser):
