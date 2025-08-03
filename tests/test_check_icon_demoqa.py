93e5a774205505e035df0b1dfc89d84a147b8878
war-julia <war-julia@yandex.ru>
2025-08-03 19:09:18 +0300

    second commit

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,25 +1,11 @@
+from pages.demoqa import DemoQa
 
-# #import time
-# from pages.demoqa import DemoQa
-#
-# def test_check_icon(browser):
-#
-#     demo_qa_page = DemoQa(browser)
-#     demo_qa_page.visit()
-#     # time.sleep(3)
+def test_check_icon(browser):
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+
 #     demo_qa_page.icon.click()
-#     # time.sleep(3)
+
 #     assert demo_qa_page.equal_url()
-#     assert demo_qa_page.exist_icon()
-
-from selenium.webdriver.common.by import By
-
-def test_icon_exist(browser):
-
-    browser.get('https://demoga.com/')
-    icon = browser.find_element(By.CSS_SELECTOR, '#app > header> a')
-    if icon is None:
-        print("Элемент не найден")
-    else:
-        print("Элемент найден")
+     assert demo_qa_page.exist_icon()
 
