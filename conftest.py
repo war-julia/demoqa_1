b15b359a823f29cd208f12df9219bed50954c64a
war-julia <war-julia@yandex.ru>
2025-08-03 18:36:56 +0300

    first commit

--- conftest.py
+++ conftest.py
@@ -1,30 +1,13 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='session')
-def driver():
-    """Fixture to create and manage Chrome WebDriver instance for Ломоносовская гимназия testing"""
-    chrome_options = Options()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-web-security")
-    chrome_options.add_argument("--allow-running-insecure-content")
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
+def browser():
     
-    # Set user agent to avoid detection
-    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-    
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome()
     driver.implicitly_wait(10)
     driver.maximize_window()
-    
-    # Execute script to remove webdriver property
-    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     
     yield driver
     driver.quit()
--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -11,3 +11,15 @@
 #     # time.sleep(3)
 #     assert demo_qa_page.equal_url()
 #     assert demo_qa_page.exist_icon()
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def test_icon_exist():
+    driver = webdriver.Chrome()
+    driver.get('https://demoga.com/')
+    icon = driver.find_element(By.CSS_SELECTOR, '#app > header> a')
+    if icon is None:
+        print("Элемент не найден")
+    else:
+        print("Элемент найден")
+
