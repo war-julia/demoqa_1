f0926f88d8e9583a833bc3072c13041bd9b6eba5
war-julia <war-julia@yandex.ru>
2025-08-03 13:58:16 +0300

    first commit

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,21 +1,21 @@
-from selenium.webdriver.common.by import By
-
-
-class BasePage:
-    """Base page class that contains common methods for all pages"""
-
-    def __init__(self, driver, base_url):
-        self.driver = driver
-        self.base_url = base_url
-
-    def visit(self):
-        """Navigate to the base URL"""
-        return self.driver.get(self.base_url)
-
-    def get_url(self):
-        """Get current page URL"""
-        return self.driver.current_url
-
-    def equal_url(self):
-        """Check if current URL matches base URL"""
-        return self.get_url() == self.base_url
+# from selenium.webdriver.common.by import By
+#
+#
+# class BasePage:
+#     """Base page class that contains common methods for all pages"""
+#
+#     def __init__(self, driver, base_url):
+#         self.driver = driver
+#         self.base_url = base_url
+#
+#     def visit(self):
+#         """Navigate to the base URL"""
+#         return self.driver.get(self.base_url)
+#
+#     def get_url(self):
+#         """Get current page URL"""
+#         return self.driver.current_url
+#
+#     def equal_url(self):
+#         """Check if current URL matches base URL"""
+#         return self.get_url() == self.base_url
