3e1716728ec93cf09726663463e89abd80bc48f4
war-julia <war-julia@yandex.ru>
2025-08-10 00:38:37 +0300

    seventh commit

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -36,6 +36,7 @@ def test_check_text_please(browser):
 
 def test_page_elements(browser):
     elements_page = ElementsPage(browser)
-
     elements_page.visit()
-    assert elements_page.text_elements.get_text() == "Please select an item from left to start practice."
+    assert elements_page.icon.exist()
+    assert elements_page.btn_sidebar_first.exist()
+    assert elements_page.btn_sidebar_first_textbox.exist()
