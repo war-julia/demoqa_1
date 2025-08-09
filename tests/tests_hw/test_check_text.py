5a421b29a7443a595361de52ba229bb008367040
war-julia <war-julia@yandex.ru>
2025-08-09 20:26:55 +0300

    sixth commit

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -38,3 +38,4 @@ def test_page_elements(browser):
     elements_page = ElementsPage(browser)
 
     elements_page.visit()
+    assert elements_page.text_elements.get_text() == "Please select an item from left to start practice."
