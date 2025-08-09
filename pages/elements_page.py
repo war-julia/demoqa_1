d81761a57894c180eaeefafd7aed812340124d51
war-julia <war-julia@yandex.ru>
2025-08-09 20:15:41 +0300

    sixth commit

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,8 +1,15 @@
 from pages.base_page import BasePage
+from components.components import WebElement
+
 
 class ElementsPage(BasePage):
 
     def __init__(self,driver):
         self.base_url= 'https://demoqa.com/elements'
         super().__init__(driver,self.base_url)
+
+        self.text_please = WebElement(driver, ".col-12.mt-4.col-md-6")
+        self.text_elements = WebElement(driver, "div")
+
+
 
--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -1,39 +1,40 @@
-# #Создайте директорию tests_hw в папке tests все домашние тесты создавайте в ней
-# 1. в классе компонентов создайте метод для получения текста с элементов get_text(self).
-#  текст из элемента считывайте так str(self.find_element().text)
-# 2. в файле test_check_text.py реализуйте таст кейс:
-# a. перейти на страницу 'https://demoqa.com/'
-# b. проверить что текст в подвале == ‘© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.’
-
 from pages.demoqa import DemoQa
 from pages.elements_page import ElementsPage
 from selenium.webdriver.common.by import By
 
 
-def test_check_copyright_text(browser):
+def test_check_footer(browser):
     demo_qa_page = DemoQa(browser)
     demo_qa_page.visit()
-    assert demo_qa_page.equal_url()
+    # assert demo_qa_page.equal_url()
     # поиск footer
-    footer = browser.find_element(By.CSS_SELECTOR, "footer")
+    # footer = browser.find_element(By.CSS_SELECTOR, "footer")
     # проверка copyright text
-    copyright_text = "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."
+    # footer = "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."
 
-    assert copyright_text in footer.text, "Copyright text not found in footer"
+    # assert footer in footer.text, "Copyright text not found in footer"
+    assert demo_qa_page.text_footer.get_text() == "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."
 
 
-def test_check_text_page_elements(browser):
+
+def test_check_text_please(browser):
     demo_qa_page = DemoQa(browser)
     elements_page = ElementsPage(browser)
+
     demo_qa_page.visit()
-    assert demo_qa_page.equal_url()
+    # assert demo_qa_page.equal_url()
     demo_qa_page.btn_elements.click()
-    assert elements_page.equal_url()
+    # assert elements_page.equal_url()
     
     # center text
-    center_element = browser.find_element(By.CSS_SELECTOR, ".col-12.mt-4.col-md-6")
+    # center_element = browser.find_element(By.CSS_SELECTOR, ".col-12.mt-4.col-md-6")
     
     #  проверка center text
-    expected_text = "Please select an item from left to start practice."
-    assert expected_text in center_element.text, "Center text not found"
+    # expected_text = "Please select an item from left to start practice."
+    # assert expected_text in center_element.text, "Center text not found"
+    assert elements_page.text_please.get_text() == "Please select an item from left to start practice."
 
+def test_page_elements(browser):
+    elements_page = ElementsPage(browser)
+
+    elements_page.visit()
