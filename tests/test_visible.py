2cf089f59a48838620c24fd08b553543c07d6135
war-julia <war-julia@yandex.ru>
2025-08-10 09:03:26 +0300

    eight commit

--- tests/test_visible.py
+++ tests/test_visible.py
@@ -5,9 +5,10 @@ def test_visible_btn_sidebar(browser):
     elements_page = ElementsPage(browser)
 
     elements_page.visit()
-    elements_page.btn_sidebar_first.click()
-    time.sleep(3)
-    assert elements_page.btn_sidebar_first_textbox.exist()
+    # elements_page.btn_sidebar_first.click()
+    # time.sleep(3)
+    # assert elements_page.btn_sidebar_first_textbox.exist()
+    assert elements_page.btn_sidebar_first_textbox.visible()
 
 
 
