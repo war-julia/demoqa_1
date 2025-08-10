5f6ac8c06f8a6e17a3ac1f4781b81896c0f41d93
war-julia <war-julia@yandex.ru>
2025-08-10 08:57:47 +0300

    eight commit

--- components/components.py
+++ components/components.py
@@ -5,31 +5,28 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 class WebElement:
-    def __init__(self, driver, locator='', timeout=10):
+    def __init__(self, driver, locator=''):
         self.driver = driver
         self.locator = locator
-        self.timeout = timeout
-        self.wait = WebDriverWait(driver, timeout)
 
     def click(self):
-        element = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, self.locator)))
-        element.click()
+        self.find_element.click()
 
     def find_element(self):
-        return self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, self.locator)))
+        return self.driver.find_element(By.CSS_SELECTOR, self.locator)
     
     def exist(self):
         try:
             self.find_element()
             return True
-        except (NoSuchElementException, TimeoutException):
+        except NoSuchElementException, TimeoutException:
             return False
-
+        return True
 
     def get_text(self):
-        try:
-            str(self.find_element().text)
-            return True
-        except (NoSuchElementException, TimeoutException):
-            return False
+        return str(self.find_element().text)
+
+
+    def visible(self):
+        return self.find_element().is_displayed()
 
