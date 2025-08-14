e38af864229ec11702a5f4f4346c5f8664f8bfd6
war-julia <war-julia@yandex.ru>
2025-08-14 08:11:58 +0300

    14 commit

--- conftest.py
+++ conftest.py
@@ -5,30 +5,11 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope='session')
 def browser():
-    # """Create and configure Chrome WebDriver"""
-    # chrome_options = Options()
-    # chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--disable-web-security")
-    # chrome_options.add_argument("--allow-running-insecure-content")
-    # chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    # chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    # chrome_options.add_experimental_option('useAutomationExtension', False)
-    #
-    # # Set user agent to avoid detection
-    # chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-    #
-    # driver = webdriver.Chrome(options=chrome_options)
-    driver = webdriver.Chrome()
-    # driver.implicitly_wait(10)
-    # driver.maximize_window()
 
+    driver = webdriver.Chrome()
+    driver.set_window_size(1000, 1000)
     
-    # Execute script to remove webdriver property
-    # driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-    
+
     yield driver
-    browser.set_window_size(1000, 1000)
 
     driver.quit()
