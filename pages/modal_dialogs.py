adae11adbf2012be8f6cd15ddaf47b858f7c7d05
war-julia <war-julia@yandex.ru>
2025-08-15 19:30:56 +0300

    19 commit

--- pages/modal_dialogs.py
+++ pages/modal_dialogs.py
@@ -8,8 +8,6 @@ class ModalDialogs(BasePage):
         self.base_url = 'https://demoqa.com/modal-dialogs'
         super().__init__(driver, self.base_url)
 
-        # Element for all submenu buttons (5 buttons total)
         self.btns_submenu = WebElement(driver, ".btn")
-        
-        # Element for the icon to navigate to main page
+
         self.icon = WebElement(driver, "header > a > img")
--- tests/test_visible.py
+++ tests/test_visible.py
@@ -1,22 +1,22 @@
-from pages.elements_page import ElementsPage
-
-def test_visible_btn_sidebar(browser):
-    elements_page = ElementsPage(browser)
-
-    elements_page.visit()
-    # Click the sidebar button first to expand it
-    elements_page.btn_sidebar_first.click()
-    # Then check if the textbox is visible
-    assert elements_page.btn_sidebar_first_textbox.visible()
-
-
-def test_not_visible_btn_sidebar(browser):
-    elements_page = ElementsPage(browser)
-
-    elements_page.visit()
-    # Initially the checkbox should be visible (sidebar is expanded by default)
-    assert elements_page.btn_sidebar_first_checkbox.visible()
-    # Click to collapse the sidebar
-    elements_page.btn_sidebar_first.click()
-    # After clicking, the checkbox should be hidden
-    assert not elements_page.btn_sidebar_first_checkbox.visible()
+# from pages.elements_page import ElementsPage
+#
+# def test_visible_btn_sidebar(browser):
+#     elements_page = ElementsPage(browser)
+#
+#     elements_page.visit()
+#     # Click the sidebar button first to expand it
+#     elements_page.btn_sidebar_first.click()
+#     # Then check if the textbox is visible
+#     assert elements_page.btn_sidebar_first_textbox.visible()
+#
+#
+# def test_not_visible_btn_sidebar(browser):
+#     elements_page = ElementsPage(browser)
+#
+#     elements_page.visit()
+#     # Initially the checkbox should be visible (sidebar is expanded by default)
+#     assert elements_page.btn_sidebar_first_checkbox.visible()
+#     # Click to collapse the sidebar
+#     elements_page.btn_sidebar_first.click()
+#     # After clicking, the checkbox should be hidden
+#     assert not elements_page.btn_sidebar_first_checkbox.visible()
