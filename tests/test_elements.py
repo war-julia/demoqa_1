d6ff2c5f95aff1174c830a04feb67be8c52454f2
war-julia <war-julia@yandex.ru>
2025-08-15 13:06:50 +0300

    15 commit

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -4,7 +4,7 @@ def test_find_elements(browser):
     elements_page = ElementsPage(browser)
     elements_page.visit()
 
-    assert
+    assert elements_page.btns_first_menu.check_count_elements(count=9)
 
 
 
