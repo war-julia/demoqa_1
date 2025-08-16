84583c29f9dc5659a6b5792f4c8dfb268a48e681
war-julia <war-julia@yandex.ru>
2025-08-16 17:15:11 +0300

    22 commit

--- tests/tests_hw/test_page_dialogs.py
+++ tests/tests_hw/test_page_dialogs.py
@@ -4,30 +4,20 @@ from pages.demoqa import DemoQa
 
 def test_modal_elements(browser):
     modal_dialogs_page = ModalDialogs(browser)
-
     modal_dialogs_page.visit()
 
     assert modal_dialogs_page.btns_submenu.check_count_elements(count=2)
 
-
 def test_navigation_modal(browser):
     modal_dialogs_page = ModalDialogs(browser)
     demo_qa_page = DemoQa(browser)
-
     modal_dialogs_page.visit()
-
     browser.refresh()
-
     modal_dialogs_page.icon.click()
-
     browser.back()
-
     browser.set_window_size(900, 400)
-
     browser.forward()
 
     assert demo_qa_page.equal_url()
-
     assert browser.title == 'DEMOQA'
-
     browser.set_window_size(1000, 1000)
