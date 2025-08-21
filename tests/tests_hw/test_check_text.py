74e78ce1df4d64870a8cff5d5eb9732b947f2d95
war-julia <war-julia@yandex.ru>
2025-08-21 19:48:34 +0300

    32 commit

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -7,7 +7,6 @@
 #     demo_qa_page.visit()
 #     assert demo_qa_page.text_footer.get_text() == "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."
 #
-#
 # def test_check_text_please(browser):
 #     demo_qa_page = DemoQa(browser)
 #     elements_page = ElementsPage(browser)
@@ -16,7 +15,6 @@
 #     demo_qa_page.btn_elements.click()
 #
 #     assert elements_page.text_please.get_text() == "Please select an item from left to start practice."
-#
 #
 # def test_page_elements(browser):
 #     elements_page = ElementsPage(browser)
