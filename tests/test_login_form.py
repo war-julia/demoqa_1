22d9fb47217d5cff3e630bdbc1e19ed4b0fe717b
war-julia <war-julia@yandex.ru>
2025-08-21 19:18:17 +0300

    32 commit

--- tests/test_login_form.py
+++ tests/test_login_form.py
@@ -1,31 +1,31 @@
-from pages.form_page import FormPages
-import time
-
-def test_login_form(browser):
-    form_page = FormPages(browser)
-    form_page.visit()
-    assert not form_page.modal_dialog.exist()
-    form_page.first_name.send_keys("tester")
-    form_page.last_name.send_keys("testerovich")
-    form_page.user_email.send_keys("test@ttt.tt")
-    form_page.gender_radio_1.click_force()
-    form_page.user_number.send_keys("1111111111")
-    form_page.hobbies.click_force()
-    form_page.current_address.send_keys("karelia")
-    form_page.btn_submit.click_force()
-    assert form_page.modal_dialog.exist()
-    form_page.btn_close_modal.click_force()
-
-def test_login_form_state_city(browser):
-    form_page = FormPages(browser)
-    form_page.visit()
-    time.sleep(2)
-    form_page.state.click_with_scroll()
-    time.sleep(1)
-    form_page.state_option.click_force()
-    form_page.city.click_with_scroll()
-    time.sleep(1)
-    form_page.city_option.click_force()
-
-    assert form_page.state.get_text() != ""
-    assert form_page.city.get_text() != ""
+# from pages.form_page import FormPages
+# import time
+#
+# def test_login_form(browser):
+#     form_page = FormPages(browser)
+#     form_page.visit()
+#     assert not form_page.modal_dialog.exist()
+#     form_page.first_name.send_keys("tester")
+#     form_page.last_name.send_keys("testerovich")
+#     form_page.user_email.send_keys("test@ttt.tt")
+#     form_page.gender_radio_1.click_force()
+#     form_page.user_number.send_keys("1111111111")
+#     form_page.hobbies.click_force()
+#     form_page.current_address.send_keys("karelia")
+#     form_page.btn_submit.click_force()
+#     assert form_page.modal_dialog.exist()
+#     form_page.btn_close_modal.click_force()
+#
+# def test_login_form_state_city(browser):
+#     form_page = FormPages(browser)
+#     form_page.visit()
+#     time.sleep(2)
+#     form_page.state.click_with_scroll()
+#     time.sleep(1)
+#     form_page.state_option.click_force()
+#     form_page.city.click_with_scroll()
+#     time.sleep(1)
+#     form_page.city_option.click_force()
+#
+#     assert form_page.state.get_text() != ""
+#     assert form_page.city.get_text() != ""
--- tests/tests_hw/test_text_box.py
+++ tests/tests_hw/test_text_box.py
@@ -1,21 +1,21 @@
-from pages.text_box import TextBox
-import time
-
-
-def test_text_box_submit(browser):
-    text_box_page = TextBox(browser)
-    text_box_page.visit()
-    time.sleep(3)
-    name_text = "Ivan Ivanov"
-    address_text = "Russia Moscow Polezhaevskaya, 16-8"
-    text_box_page.name.send_keys(name_text)
-    text_box_page.current_address.send_keys(address_text)
-    text_box_page.btn_submit.scroll_to_element()
-    time.sleep(1)
-    text_box_page.btn_submit.click()
-    time.sleep(2)
-    name_result = text_box_page.name_output.get_text()
-    address_result = text_box_page.current_address_output.get_text()
-
-    assert name_result == f"Name:{name_text}"
-    assert address_result == f"Current Address :{address_text}"
+# from pages.text_box import TextBox
+# import time
+#
+#
+# def test_text_box_submit(browser):
+#     text_box_page = TextBox(browser)
+#     text_box_page.visit()
+#     time.sleep(3)
+#     name_text = "Ivan Ivanov"
+#     address_text = "Russia Moscow Polezhaevskaya, 16-8"
+#     text_box_page.name.send_keys(name_text)
+#     text_box_page.current_address.send_keys(address_text)
+#     text_box_page.btn_submit.scroll_to_element()
+#     time.sleep(1)
+#     text_box_page.btn_submit.click()
+#     time.sleep(2)
+#     name_result = text_box_page.name_output.get_text()
+#     address_result = text_box_page.current_address_output.get_text()
+#
+#     assert name_result == f"Name:{name_text}"
+#     assert address_result == f"Current Address :{address_text}"
