f44636a3b7fed5712fd0fffa0b217c1b89ce429e
war-julia <war-julia@yandex.ru>
2025-08-22 13:38:34 +0300

    33 commit

--- tests/test_webtables.py
+++ tests/test_webtables.py
@@ -6,7 +6,7 @@ def test_tables(browser):
     page_tables.visit()
 
     assert not page_tables.no_data_exist()
-    while page_tables.btn_delete_row.exist()
+    while page_tables.btn_delete_row.exist():
         page_tables.btn_delete_row.click()
     time.sleep(2)
     assert page_tables.no_data_exist()
