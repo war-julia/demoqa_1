451a262228e60290c835cf3e61f457e345c8847f
war-julia <war-julia@yandex.ru>
2025-08-22 08:19:51 +0300

    33 commit

--- tests/tests_hw/test_css.py
+++ tests/tests_hw/test_css.py
@@ -10,6 +10,10 @@ def test_text_box_submit(browser):
     
     # Check the submit button CSS with correct attribute name and format
     assert text_box.btn_submit.check_css("color", "rgba(255, 255, 255, 1)")
-    
+    assert text_box.submit.check_css('backgroundColor', 'rgba(0, 123, 255, 1)')
+    assert text_box.submit.check_css('borderColor', 'rgba(0, 123, 255)')
     # Verify button is visible
     assert text_box.btn_submit.visible()
+
+
+
