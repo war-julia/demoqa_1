82c4d54ff8943dccb777c0540a48326a27583337
war-julia <war-julia@yandex.ru>
2025-08-23 18:51:24 +0300

    43 commit

--- tests/test_alert.py
+++ tests/test_alert.py
@@ -1,37 +1,37 @@
-import time
-
-from pages.alerts import Alerts
-
-def test_alert(browser):
-    alert_page = Alerts(browser)
-    alert_page.visit()
-    assert not alert_page.alert()
-    alert_page.alertButton.click()
-    time.sleep(2)
-    assert alert_page.alert()
-
-def test_alert_text(browser):
-    alert_page = Alerts(browser)
-    alert_page.visit()
-    alert_page.alertButton.click()
-    assert alert_page.alert().text == "You clicked a button"
-    alert_page.alert().accept()
-    assert not alert_page.alert()
-
-def test_confirm(browser):
-    alert_page = Alerts(browser)
-    alert_page.visit()
-    alert_page.confirmButton.click()
-    time.sleep(2)
-    alert_page.alert().dismiss()
-    assert alert_page.confirmResult.get_text() == "You selected Cancel"
-
-def test_prompt(browser):
-    alert_page = Alerts(browser)
-    name = "Artem"
-    alert_page.visit()
-    alert_page.promptButton.click()
-    time.sleep(2)
-    alert_page.alert().send_keys(name)
-    alert_page.alert().accept()
-    assert alert_page.promptResult.get_text() == f"You entered {name}"
+# import time
+#
+# from pages.alerts import Alerts
+#
+# def test_alert(browser):
+#     alert_page = Alerts(browser)
+#     alert_page.visit()
+#     assert not alert_page.alert()
+#     alert_page.alertButton.click()
+#     time.sleep(2)
+#     assert alert_page.alert()
+#
+# def test_alert_text(browser):
+#     alert_page = Alerts(browser)
+#     alert_page.visit()
+#     alert_page.alertButton.click()
+#     assert alert_page.alert().text == "You clicked a button"
+#     alert_page.alert().accept()
+#     assert not alert_page.alert()
+#
+# def test_confirm(browser):
+#     alert_page = Alerts(browser)
+#     alert_page.visit()
+#     alert_page.confirmButton.click()
+#     time.sleep(2)
+#     alert_page.alert().dismiss()
+#     assert alert_page.confirmResult.get_text() == "You selected Cancel"
+#
+# def test_prompt(browser):
+#     alert_page = Alerts(browser)
+#     name = "Artem"
+#     alert_page.visit()
+#     alert_page.promptButton.click()
+#     time.sleep(2)
+#     alert_page.alert().send_keys(name)
+#     alert_page.alert().accept()
+#     assert alert_page.promptResult.get_text() == f"You entered {name}"
--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1,13 +1,13 @@
-from pages.browser_tab import BrowserTab
-import time
-def test_browser_tab(browser):
-    page_browser = BrowserTab(browser)
-    page_browser.visit()
-
-    assert len(browser.window_handles) == 1
-    page_browser.new_tab.click()
-    time.sleep(2)
-    assert len(browser.window_handles) == 2
-
-    browser.switch_to.window(browser.window_handles[2])
-    time.sleep(2)
+# from pages.browser_tab import BrowserTab
+# import time
+# def test_browser_tab(browser):
+#     page_browser = BrowserTab(browser)
+#     page_browser.visit()
+#
+#     assert len(browser.window_handles) == 1
+#     page_browser.new_tab.click()
+#     time.sleep(2)
+#     assert len(browser.window_handles) == 2
+#
+#     browser.switch_to.window(browser.window_handles[2])
+#     time.sleep(2)
