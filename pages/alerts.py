e8250b64e2e4437fb08e5eb850c9fc0725e18bf5
war-julia <war-julia@yandex.ru>
2025-08-23 17:48:15 +0300

    42 commit

--- pages/alerts.py
+++ pages/alerts.py
@@ -2,10 +2,13 @@ from selenium.common.exceptions import NoSuchElementException
 from pages.base_page import BasePage
 from components.components import WebElement
 
-class Alert(BasePage):
+class Alerts(BasePage):
 
     def __init__(self, driver):
         self.base_url = 'https://demoqa.com/alerts'
         super().__init__(driver, self.base_url)
 
-        self.alert = WebElement(driver, '#alertButton')
+        self.alertButton = WebElement(driver, '#alertButton')
+        self.confirmResult = WebElement(driver, '#confirmResult')
+        self.promptButton = WebElement(driver, '#promptButton')
+        self.promptResult = WebElement(driver, '#promptResult')
--- tests/test_alert.py
+++ tests/test_alert.py
@@ -1,6 +1,19 @@
-# from pages.alerts import Alerts:
-#
-# def test_allert(browser):
-#     alert_page = Alerts(browser)
-#     alert_page.visit()
-#     assert
+import time
+
+from pages.alerts import Alerts:
+
+def test_allert(browser):
+    alert_page = Alerts(browser)
+    alert_page.visit()
+    assert not alert_page.alert()
+    alert_page.alertButton.click()
+    time.sleep(2)
+    assert alert_page.alert()
+
+def test_alert_text(browser):
+    alert_page = Alerts(browser)
+    alert_page.visit()
+    alert_page.alertButton.click()
+    assert alert_page.alert().text == "You clicked a button"
+    alert_page.alert().accept()
+    assert not alert_page.alert()
