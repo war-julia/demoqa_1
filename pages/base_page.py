d93ee14955de09ebc40f9d6fc64702811661766f
war-julia <war-julia@yandex.ru>
2025-08-25 13:25:08 +0300

    45 commit

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
 import logging
 import time
-
+from components.components import WebElement
 class BasePage:
     def __init__(self, driver, base_url):
         self.driver = driver
         self.base_url = base_url
+        self.viewport = WebElement(driver, "head > meta")
 
     def visit(self):
         try:
--- tests/test_seo_meta.py
+++ tests/test_seo_meta.py
@@ -1,18 +1,15 @@
 # import time
-#
 # from pages.demoqa import DemoQa
 # from pages.accordion import Accordion
 # from pages.alerts import Alerts
 # from pages.browser_tab import BrowserTab
 # import pytest
-# def test_check_title_demo(browser):
-#     demo_qa_page = DemoQa(browser)
 #
-#     demo_qa_page.visit()
-#     assert browser.title == 'DEMOQA'
-#  @pytest.mark.parametrize("pages", [Accordion, Alerts, DemoQa, BrowserTab])
+# @pytest.mark.parametrize("pages", [Accordion, Alerts, DemoQa, BrowserTab])
 # def test_check_title_all_pages(browser, pages):
 #     page = pages(browser)
 #     page.visit()
 #     time.sleep(2)
-#     assert page.get_title() == 'DEMOQA'
+#     assert page.viewport.exist()
+#     assert page.viewport.get_dom_attribute("name") == "viewport"
+#     assert page.get_dom_attribute("content") == "width=device-width,initial-scale=1"
