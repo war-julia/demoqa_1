6f49b0a1007f75f2aa0228cae89e68c06186a3b3
war-julia <war-julia@yandex.ru>
2025-08-25 13:51:02 +0300

    48 commit

--- tests/test_progress_bar.py
+++ tests/test_progress_bar.py
@@ -1,14 +1,14 @@
-import time
-
-
-def test_progress_bar(browser):
-    page = Progress(browser)
-
-    page.visit()
-    time.sleep(2)
-    page.button.click()
-    while True:
-        if page.bar.get_dom_attribute("aria-valuenow") == "51"
-            page.button.click()
-            break
-    time.sleep(2)
+# import time
+#
+#
+# def test_progress_bar(browser):
+#     page = Progress(browser)
+#
+#     page.visit()
+#     time.sleep(2)
+#     page.button.click()
+#     while True:
+#         if page.bar.get_dom_attribute("aria-valuenow") == "51"
+#             page.button.click()
+#             break
+#     time.sleep(2)
