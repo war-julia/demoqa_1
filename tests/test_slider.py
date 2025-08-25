0e0e6973a9e4601434c3f8f5a0ada675bac058cf
war-julia <war-julia@yandex.ru>
2025-08-25 13:43:49 +0300

    47 commit

---
+++ tests/test_slider.py
@@ -0,0 +1,8 @@
+# def test_slider_v1(browser):
+#     slider = Slider(browser)
+#     slider.visit()
+#     assert slider.slider.exist()
+#     assert slider.inp.exist()
+#     slider.inp.send_keys("50")
+#     sleder.slider.send_keys("50")
+#     assert slider.ino.get_dom_attribute("value") == "50"
